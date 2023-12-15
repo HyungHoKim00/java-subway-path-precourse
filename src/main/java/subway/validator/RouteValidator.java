dd4ac9bc0576ea5e22b83e23f218f1abed4942b6
HyungHoKim <hkim1109@naver.com>
2023-12-15 14:51:47 +0900

    feat(RouteValidator): 경로 예외 처리
    거리가 존재하지 않을 시 예외 처리하는 validateByRoute 구현 및 이에 따른 validate 메서드 이름 변경

--- src/main/java/subway/validator/RouteValidator.java
+++ src/main/java/subway/validator/RouteValidator.java
@@ -1,11 +1,20 @@
 package subway.validator;
 
+import java.util.List;
+
 public class RouteValidator {
     private static final String SAME_STATION = "출발역과 도착역이 동일합니다.";
+    private static final String NO_ROUTE = "경로가 존재하지 않습니다.";
 
-    public static void validate(String startStation, String endStation) {
+    public static void validateByStation(String startStation, String endStation) {
         if (startStation.equals(endStation)) {
             throw new IllegalArgumentException(SAME_STATION);
+        }
+    }
+
+    public static void validateByRoute(List<String> bestRoute) {
+        if (bestRoute == null) {
+            throw new IllegalArgumentException(NO_ROUTE);
         }
     }
 }
