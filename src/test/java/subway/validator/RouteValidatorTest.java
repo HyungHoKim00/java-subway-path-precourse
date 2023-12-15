b4f1fd9396bb2fc029ea92780ada1ee5cf4637e5
HyungHoKim <hkim1109@naver.com>
2023-12-15 16:24:58 +0900

    feat(RouteValidatorTest): 경로 검증 테스트
    출발지와 도착지 간의 이동이 불가능할 시 예외 처리하는지 테스트코드 작성

--- src/test/java/subway/validator/RouteValidatorTest.java
+++ src/test/java/subway/validator/RouteValidatorTest.java
@@ -2,8 +2,11 @@ package subway.validator;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import subway.domain.RouteRepository;
 
 public class RouteValidatorTest {
     @DisplayName("출발역과 도착역이 같을 시 예외 반환")
@@ -12,6 +15,23 @@ public class RouteValidatorTest {
         String station = "선릉역";
 
         assertThatThrownBy(() -> RouteValidator.validateByStation(station, station))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("경로가 없을 시 예외 반환")
+    @Test
+    void InvalidRouteThrowsException() {
+        List<String> stationNames = new ArrayList<>(List.of(
+                "교대역", "강남역", "역삼역", "선릉역"
+        ));
+        RouteRepository.addAllStationsByName(stationNames);
+        List<String> routeDetails = new ArrayList<>(List.of(
+                "교대역,강남역,2,3", "강남역,역삼역,2,3", "교대역,역삼역,5,5"
+        ));
+        RouteRepository.addAllRoutes(routeDetails);
+
+        assertThatThrownBy(() -> RouteRepository
+                .selectRouteBySelectionCriteria("1", new String[]{"교대역", "선릉역"}))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
