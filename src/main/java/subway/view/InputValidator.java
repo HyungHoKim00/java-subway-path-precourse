bdfc4311fc799046e14c6453edc43c6416647a97
HyungHoKim <hkim1109@naver.com>
2023-12-15 12:59:51 +0900

    feat(InputValidator): 입력 예외 처리
    입력 예외를 처리하는 메서드 validateViewOrExit, validateSelectionCriteria 구현

---
+++ src/main/java/subway/view/InputValidator.java
@@ -0,0 +1,19 @@
+package subway.view;
+
+public class InputValidator {
+    private static final String VIEW_OR_EXIT_REGEX = "^([1Q])$";
+    private static final String SELECTION_CRITERIA_REGEX = "^([12Q])$";
+    private static final String INVALID_INPUT = "잘못된 입력입니다.";
+
+    public static void validateViewOrExit(String viewOrExit) {
+        if (!viewOrExit.matches(VIEW_OR_EXIT_REGEX)) {
+            throw new IllegalArgumentException(INVALID_INPUT);
+        }
+    }
+
+    public static void validateSelectionCriteria(String selectionCriteria) {
+        if (!selectionCriteria.matches(SELECTION_CRITERIA_REGEX)) {
+            throw new IllegalArgumentException(INVALID_INPUT);
+        }
+    }
+}
