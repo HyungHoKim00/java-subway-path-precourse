c6925737b1841a468196dc0bd30ca49c4675938c
HyungHoKim <hkim1109@naver.com>
2023-12-15 15:01:09 +0900

    feat(OutputView): 기능 목록 출력
    메인 화면을 출력하는 printSelectionCriteriaRequestMessage 생성

--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -7,6 +7,8 @@ public class OutputView {
     private static final String LINE = "---";
 
     public void printRoute(List<String> routeDetails) {
+        System.out.println();
+        printTitle("조회 결과");
         printInfo(LINE);
         printInfo("총 거리: " + routeDetails.get(routeDetails.size() - 2) + "km");
         printInfo("총 소요 시간: " + routeDetails.get(routeDetails.size() - 1) + "분");
@@ -20,15 +22,28 @@ public class OutputView {
         printTitle("메인 화면");
         System.out.println("1. 경로 조회");
         System.out.println("Q. 종료");
+        System.out.println();
+        printTitle("원하는 기능을 선택하세요.");
     }
 
     public void printSelectionCriteriaRequestMessage() {
+        System.out.println();
+        printTitle("경로 기준");
+        System.out.println("1. 최단 거리");
+        System.out.println("2. 최소 시간");
+        System.out.println("B. 돌아가기");
+        System.out.println();
+        printTitle("원하는 기능을 선택하세요.");
     }
 
     public void printStartStationRequestMessage() {
+        System.out.println();
+        printTitle("출발역을 입력하세요.");
     }
 
     public void printEndStationRequestMessage() {
+        System.out.println();
+        printTitle("도착역을 입력하세요.");
     }
 
 
