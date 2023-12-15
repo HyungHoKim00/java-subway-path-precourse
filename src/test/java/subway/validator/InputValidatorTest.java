904f561eb1a0234f3e9ddf1c13e1877f98ac0cb3
HyungHoKim <hkim1109@naver.com>
2023-12-15 15:48:03 +0900

    feat(InputValidatorTest): 입력 검증 테스트
    station 입력이 존재하지 않는 station일 때 예외 처리하는지 테스트코드 작성

--- src/test/java/subway/validator/InputValidatorTest.java
+++ src/test/java/subway/validator/InputValidatorTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import subway.domain.Station;
+import subway.domain.StationRepository;
 
 public class InputValidatorTest {
 
@@ -18,6 +20,17 @@ public class InputValidatorTest {
     @Test
     void invalidSelectionCriteriaThrowsException() {
         assertThatThrownBy(() -> InputValidator.validateSelectionCriteria("3"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("Station 입력테스트")
+    @Test
+    void invalidStationThrowsException() {
+        String stationName = "선릉역";
+
+        StationRepository.addStation(new Station(stationName));
+
+        assertThatThrownBy(() -> InputValidator.validateStation("강남역"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
